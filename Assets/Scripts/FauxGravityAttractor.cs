ded02609d56dcfb44c4889d0e1aa5fc98bcaa1c3
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-04-22 11:10:53 +0800

    gravity

---
+++ Assets/Scripts/FauxGravityAttractor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityAttractor : MonoBehaviour {
+
+    public float gravity = 10;
+
+	public void Attract(Transform body, bool rotateUpwardGravity) {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * -gravity);
+
+		if (rotateUpwardGravity) {
+			Quaternion targetRotation = Quaternion.FromToRotation (bodyUp, gravityUp) * body.rotation;
+			body.rotation = Quaternion.Slerp (body.rotation, targetRotation, 50 * Time.deltaTime);
+		}
+    }
+
+    public void Attract(Transform body, Vector3 facing) {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+        Vector3 bodyForward = body.forward;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * -gravity);
+
+        Vector3 dirLeft = Vector3.Cross(gravityUp, facing).normalized;
+        Vector3 dirForward = Vector3.Cross(dirLeft, gravityUp).normalized;
+        dirForward.x *= -1;
+
+        Quaternion upwardRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyForward, dirForward) * upwardRotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FauxGravityBody : MonoBehaviour {
+
+    public FauxGravityAttractor attractor;
+	public bool rotateUpwardGravity = true;
+	[ConditionalHide("rotateUpwardGravity")]
+    public bool alwaysFacing;
+	[ConditionalHide("alwaysFacing", true)]
+    public Vector2 facingDirection;
+
+    private Transform myTransform;
+
+	void Start () {
+        if(attractor == null) {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            attractor = player.GetComponent<FauxGravityAttractor>();
+        }
+
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+
+        if(alwaysFacing) {
+            facingDirection = facingDirection.normalized;
+        }
+    }
+	
+	void Update () {
+		if (rotateUpwardGravity) {
+			attractor.Attract (myTransform, rotateUpwardGravity);
+		} else if (alwaysFacing) {
+			attractor.Attract (myTransform, facingDirection);
+		} else {
+			attractor.Attract (myTransform, rotateUpwardGravity);
+		}
+	}
+
+	void OnValidate() {
+
+	}
+}
