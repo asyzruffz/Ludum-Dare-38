6ab7c5578d5f2585b622117434817b1cc11a548d
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-04-22 12:24:34 +0800

    player control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1;
+
+	private Vector3 dir;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		dir = (new Vector3 (h, 0, v)).normalized;
+	}
+
+	void FixedUpdate () {
+		GetComponent<Rigidbody> ().MovePosition (transform.position + transform.TransformDirection (dir) * speed * Time.deltaTime);
+	}
+}
