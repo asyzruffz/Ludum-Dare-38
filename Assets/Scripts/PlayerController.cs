5583e39fa1c0cdcc178204cec68dfedc86d28947
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-04-22 16:44:29 +0800

    Reaction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,9 +16,19 @@ public class PlayerController : MonoBehaviour {
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
 		dir = (new Vector3 (h, 0, v)).normalized;
+		if(Mathf.Abs(h) > 0) {
+			//transform.Rotate(0, Mathf.Cos (localDir.x) * 90, 0);
+		}
+
+		PlayerInteraction interaction = GetComponent<PlayerInteraction> ();
+		if (Input.GetButtonDown("Jump") && interaction.detecting) {
+			interaction.detectTarget.GetComponent<Reaction> ().ReactTowards (transform.position);
+			Debug.Log ("Interacting " + interaction.detectTarget.name);
+		}
 	}
 
 	void FixedUpdate () {
-		GetComponent<Rigidbody> ().MovePosition (transform.position + transform.TransformDirection (dir) * speed * Time.deltaTime);
+		Vector3 localDir = transform.TransformDirection (dir);
+		GetComponent<Rigidbody> ().MovePosition (transform.position + localDir * speed * Time.deltaTime);
 	}
 }
