5389dbcc390f05f4dfde989dad1864d006ba99e3
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-04-23 16:09:42 +0800

    Menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public Button playButton;
+
+	public void SetPlayButtonDisplay (bool enabled) {
+		playButton.gameObject.SetActive (enabled);
+	}
+}
